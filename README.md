97954e212a2d0a34f26a5f38340118555f2b7619
hualuomoli <liubaoquan0831@163.com>
2016-03-15 14:32:10 +0800

    add http proxy by local cache

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-#local-http-proxy
+# 本地http代理
+
+# 缓存http请求文件到本地，减少与互联网的情请求
+- app.js    代理服务器，默认为9000
+- test.js   测试代理服务器，端口为1000
+
+# 测试
+- 启动测试服务`node test.js`
+- 启动代理服务器`node app.js`
+- 在浏览器输入`http://localhost:9000/http://localhost:1000/abc`
---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var fs = require('fs');
+var httpProxy = require('http-proxy');
+var colors = require('colors');
+var tools = require('./tools');
+
+var app = express();
+
+// create cache path
+tools.init();
+
+
+app.get('/*', function (req, res) {
+  var url = req.url;
+  if (url === '' || url === '/favicon.ico') {
+    return;
+  }
+  // fs.readFile('D:/FlashFXP.zip', function (err, data) {
+  //   res.send(data);
+  // });
+  tools.getFile(url, res);
+});
+
+//
+// Create your proxy server and set the target in the options.
+//
+httpProxy.createProxyServer({
+  target: 'http://localhost:9000'
+}).listen(8000); // See (†)
+
+var server = app.listen(9000, function (req, res) {
+  console.log('server started in '.green + 9000);
+});
